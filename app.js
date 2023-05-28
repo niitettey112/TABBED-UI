2ae86fefb7429ea922410b9db79f23e5042a4b66
Emmanuel <tettehemmanuel112@gmail.com>
2023-05-28 18:43:58 +0000

    new changes

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 const tabs = document.querySelectorAll(".tab__btn");
+const contents = document.querySelectorAll(".details__content, .tutorials__content, .code__content")
 
-tabs.forEach((tab) => {
+tabs.forEach((tab, i) => {
     tab.addEventListener('click', () => {
-        tabs.forEach(tab=>{tab.classList.remove("active")});
-        tab.classList.add('active')
+        tabs.forEach((t, index) => {
+        if (tab === t) {
+            t.classList.add("active");
+        } else {
+            t.classList.remove("active");
+        }
+        });
+        
+        contents.forEach((content, index) => {
+        if (index === i) {
+            content.classList.add("active");
+        } else {
+            content.classList.remove("active");
+        }
+        });
     });
 });
 
+// tabs.forEach((tab, index) => {
+//     // tab.addEventListener('click', () => {
+//     //     tabs.forEach((tab) => {
+//     //         tab.classList.remove("active");
+//     //         tabs[index].classList.add("active");
+//     //     });
 
+//     //     contents.forEach((content) => {
+//     //         content.classList.remove("active");
+//     //         content[index].classList.add("active")
+//     //     });
+//     // });
+// });
 
-
-
-// if (tabs.length === 3){
-//     console.log('Buttons have been added')
-// }else{
-//     console.log('missing buttons')
-// }
-
-// console.log(tabs);
